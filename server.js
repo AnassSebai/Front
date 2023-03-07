4457ce84ddf57778775059f1d22a51bbdc0e96de
Anass <sebaianass@gmail.com>
2023-03-07 17:24:58 +0100

    d

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();//initialiation de l'instance express
 const port = 3000;// le port d'ecoute 
 
 function main(){
-    // Ajouter un middleware pour servir les fichiers statiques dans le dossier "public"
+    // Ajouter un middleware pour servir les fichiers statiques dans le dossier "Front"
     app.use(express.static("Front"));
     //on démare le serveur d'une façon qu'on écoute les requêtes entrantes 
     app.listen(port,() =>{
