9f19b63772b7a701b44ffde118fd7190f4c23930
Anass <sebaianass@gmail.com>
2023-03-15 16:14:46 +0100

    dernieres mod

--- public/clients.js
+++ public/clients.js
@@ -44,19 +44,140 @@
       }
     });
   });
-});*/
+});
 
 
 function getData()
 {
-    const tableau = 
-    [{"id":1,"email":"isidro_von@hotmail.com","prenom":"Torrey","nom":"Veum","societe":"Google","date":"2014-12-25T04:06:27.981Z","pays":"Switzerland"},
-        {"id":2,"email":"frederique19@gmail.com","prenom":"Micah","nom":"Sanford","societe":"Google","date":"2014-07-03T16:08:17.044Z","pays":"Democratic People's Republic of Korea"},
-        {"id":3,"email":"fredy54@gmail.com","prenom":"Hollis","nom":"Swift","societe":"Microsoft","date":"2014-08-18T06:15:16.731Z","pays":"Tunisia"},];
-    return tableau;
+  const tableau = 
+  [{"id":1,"email":"isidro_von@hotmail.com","first":"Torrey","last":"Veum","company":"Google","created_at":"2014-12-25T04:06:27.981Z","country":"Switzerland"},
+      {"id":2,"email":"frederique19@gmail.com","first":"Micah","last":"Sanford","company":"Google","created_at":"2014-07-03T16:08:17.044Z","country":"Democratic People's Republic of Korea"},
+      {"id":3,"email":"fredy54@gmail.com","first":"Hollis","last":"Swift","company":"Microsoft","created_at":"2014-08-18T06:15:16.731Z","country":"Tunisia"},
+      {"id":4,"email":"braxton29@hotmail.com","first":"Perry","last":"Leffler","company":"Microsoft","created_at":"2014-07-10T11:31:40.235Z","country":"Chad"},
+      {"id":5,"email":"turner59@gmail.com","first":"Janelle","last":"Hagenes","company":"Amazon","created_at":"2014-04-21T15:05:43.229Z","country":"Swaziland"}];
+  return tableau;
 }
 
 addEventListener("DOMContentLoaded",function () {
-    console.log("Le document est chargé");
-    $("#client").loadTemplate($("#tmpClient"),getData());
+  console.log("Le document est chargé");
+  $("#client").loadTemplate($("#tmpClient"),getData());
 });
+===========================================================================================
+
+
+document.addEventListener("DOMContentLoaded",async function() {
+
+  //code
+  console.log("Le document est chargé");
+
+  //declare url
+  const url = "http://localhost:3001/api/customers";
+
+  //call function to get Data
+  const resp = await getAjaxResponse(url);
+
+  //parse data 
+  //const respJson= JSON.parse(resp);
+  //console.log(respJson);
+
+  const customers = resp;
+  console.log(customers);
+  
+
+  
+  
+  document.getElementById("id").innerHTML = customers[0].id;
+
+  document.getElementById("last").innerHTML = customers[0].last;
+
+  document.getElementById("first").innerHTML = customers[0].first;
+
+  document.getElementById("company").innerHTML = customers[0].company;
+  document.getElementById("country").innerHTML = customers[0].country;
+  document.getElementById("email").innerHTML = customers[0].email;
+  document.getElementById("created_at" ).innerHTML = customers[0].created_at;
+
+
+});
+
+
+
+
+async function getAjaxResponse(url) {
+     
+  return new Promise(function(resolve, reject){
+   
+    //eslint-disable-next-line 
+    $.get(url,{},function(data,status,xhr){
+
+        if(status === "success"){
+            resolve(data);
+
+        }else{
+            reject(Error(xhr.statusText));
+        }
+    },"json");
+
+  });
+
+}
+
+
+
+*/
+
+
+document.addEventListener("DOMContentLoaded", async function() {
+
+  console.log("Le document est chargé");
+
+  const url = "http://localhost:3001/api/customers";
+
+  const resp = await getAjaxResponse(url);
+
+  const customers = resp;
+
+  const tableBody = document.getElementById("customers");
+  
+
+  for (let i = 0; i < customers.length; i++) {
+    const customer = customers[i];
+
+    const row = tableBody.insertRow();
+
+    const idCell = row.insertCell();
+    idCell.innerHTML = customer.id;
+
+    const lastCell = row.insertCell();
+    lastCell.innerHTML = customer.last;
+
+    const firstCell = row.insertCell();
+    firstCell.innerHTML = customer.first;
+
+    const companyCell = row.insertCell();
+    companyCell.innerHTML = customer.company;
+
+    const countryCell = row.insertCell();
+    countryCell.innerHTML = customer.country;
+
+    const emailCell = row.insertCell();
+    emailCell.innerHTML = customer.email;
+
+    const createdAtCell = row.insertCell();
+    createdAtCell.innerHTML = customer.created_at;
+  }
+ 
+
+});
+
+async function getAjaxResponse(url) {
+  return new Promise(function(resolve, reject) {
+    $.get(url, {}, function(data, status, xhr) {
+      if (status === "success") {
+        resolve(data);
+      } else {
+        reject(Error(xhr.statusText));
+      }
+    }, "json");
+  });
+}
