a1bc5490d276d2824120c2a163687995c4939f7c
Anass <sebaianass@gmail.com>
2023-05-05 23:28:53 +0200

    modification

--- server.js
+++ server.js
@@ -1,54 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser= require("body-parser");
 const cors = require('cors');
+//on recupere le module fs
+const app = express();//initialiation de l'application web
+const port = 3000;//port sur lequel le serveur web est lancé
 
 
-function main() {
-  const app = express();
+function main(){
 
-  app.use(bodyParser.json());
-  app.use(cors());
-  app.use(express.static(('public')));
+    app.use(express.static('public'));
+    app.use(cors());
+    app.use(bodyParser.urlencoded({extended: true}));
+     
 
-  // Tableau de clients
-  const clients = [];
 
-  // Route pour récupérer tous les clients
-  app.get('/clients', (req, res) => {
-    res.json(clients);
-  });
-
-  // Route pour ajouter un nouveau client
-  app.post('/clients', (req, res) => {
-      // Génère un ID unique pour le nouveau client
-      const newClientId = clients.length > 0 ? clients[clients.length - 1].id + 1 : 1;
-
-      // Génère automatiquement la date
-      const date = new Date().toLocaleDateString();
-
-      // Crée le nouveau client
-      const newClient = {
-        id: newClientId,
-        email: req.body.email,
-        nom: req.body.nom,
-        prenom: req.body.prenom,
-        societe: req.body.societe,
-        pays: req.body.pays,
-        date: date
-      };
-
-      // Ajoute le nouveau client au tableau
-      clients.push(newClient);
-
-      // Renvoie le nouveau client avec le code de statut 201 Created
-      res.status(201).json(newClient);
+    app.listen(port,function() {//lance le serveur web est a l'ecoute
+        console.log(`Serveur lancé sur http.//localhost: ${port}`);
     });
-
-  // Port d'écoute
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Serveur démarré sur le port ${port}`);
-  });
+ 
+    // on pourra creer une page
 }
-
 main();
+
